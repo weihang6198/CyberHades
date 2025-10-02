36d5ddc597d7871692771ab95ce7b0937821f2b8
weihang6198 <weihangmjapanlife@gmail.com>
2025-10-02 14:50:49 +0900

    animation

--- Assets/Scenes/Moh Scene/Tutorial/TutorialScripts/PlayerControllerTutorial.cs
+++ Assets/Scenes/Moh Scene/Tutorial/TutorialScripts/PlayerControllerTutorial.cs
@@ -7,34 +7,79 @@ public class PlayerControllerTutorial : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float rotationSpeed = 500f;
 
+    [Header("Ground check settings")]
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] Vector3 groundCheckOffset;
+    [SerializeField] LayerMask groundLayer;
+
+    bool isGrounded;
+    float ySpeed;
+
+    public float moveAmount;
+
     Quaternion targetRotation;
 
     CameraController cameraController;
+    Animator animator;
+    CharacterController characterController;
     private void Awake()
     {
         cameraController=Camera.main.GetComponent<CameraController>();
+        animator = GetComponent<Animator>();
+        characterController=GetComponent<CharacterController>();
     }
     private void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        float moveAmount = Mathf.Abs(h) + Mathf.Abs(v) ;
+         moveAmount = Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v));
 
         var moveInput = (new Vector3(h, 0, v)).normalized;
 
         var moveDir= cameraController.PlaneRotation* moveInput;
-        
-        //player is moving
-        if(moveAmount>0)
+
+        GroundCheck();
+        Debug.Log("GroundCheck :" + isGrounded);
+
+        if(isGrounded)
         {
-            //move the chara relative to camera
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
+            ySpeed = -0.5f;
+        }
+        else
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+            var velocity = moveDir * moveSpeed;
+        //apply gravity if player not on ground 
+        velocity.y = ySpeed;
+
+        //move the chara relative to camera,vertical movement is also applied
+        characterController.Move(velocity * Time.deltaTime);
+        //player is moving
+        if (moveAmount>0)
+        {
+           
+           
+            //transform.position += moveDir * moveSpeed * Time.deltaTime; //old 
+
             //rotate chara relative to camera 
             targetRotation = Quaternion.LookRotation(moveDir);
         }
 
        transform.rotation=Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        
+
+        animator.SetFloat("MoveAmount", moveAmount,0.2f,Time.deltaTime);
+    }
+
+    void GroundCheck()
+    {
+        isGrounded=Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
 }
