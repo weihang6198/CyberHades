01ced31c4085876a03cca7bb944228591a2faf8d
SAKVKVN <ukas.s.saku@gmail.com>
2025-10-22 00:59:21 +0900

    ProjectileFire

---
+++ Assets/VFX/Scripts/ProjectileMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    public float speed;
+    public float fireRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speed != 0)
+        {
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.Log("Speed has 0");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+        Destroy(gameObject);
+    }
+}
